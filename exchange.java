2ae2fcb6667f580037940bd49c14b3a9e9c5e00a
jingyu0 <1404637683@qq.com>
2019-12-02 21:41:48 +0800

    Java实现大小写互换与进制转换

---
+++ exchange.java
@@ -0,0 +1,33 @@
+//public class Main {
+//    public static void main(String[] args) {
+//        //将数字-1234567转化为二进制，八进制与十六进制
+//        System.out.println("-1234567的二进制为："+Integer.toBinaryString(-1234567));
+//        System.out.println("-1234567的八进制为："+Integer.toOctalString(-1234567));
+//        System.out.println("-1234567的十六进制为："+Integer.toHexString(-1234567));
+//    }
+//}
+
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        //实现一个字符串中大小写的转化
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("请输入一个字符串:");
+//        String line = sc.nextLine();
+//        //把字符串转化为字符数组
+//        char[] chs=line.toCharArray();
+//        for(int i = 0; i<chs.length ; i++){
+//            char ch=chs[i];
+//            if(Character.isUpperCase(ch)){
+//                //public static char toLowerCase(char ch):把给定的字符转化为小写字符
+//                System.out.print(Character.toLowerCase(ch));
+//            }
+//            else if(Character.isLowerCase(ch)){
+//                //public static char toUpperCase(char ch):把给定的字符转化为大写字符
+//                System.out.print(Character.toUpperCase(ch));
+//            }
+//        }
+//    }
+//}
+
